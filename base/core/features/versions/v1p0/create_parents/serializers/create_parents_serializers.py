d623f9b1ecbad6cc966970ee63e935b220d593e1
JohnCarlS <salaojohn4@gmail.com>
2024-02-07 14:28:39 +0800

    Added Create Parents features

---
+++ base/core/features/versions/v1p0/create_parents/serializers/create_parents_serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from petlandia.models import Parent
+
+class CreateParentSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Parent
+        fields = ['first_name', 'last_name', 'occupation', 'contact_number']
+        
