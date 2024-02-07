69b7dde854419a14100b69ed44225259ef1d7fc3
JohnCarlS <salaojohn4@gmail.com>
2024-02-07 14:31:47 +0800

    added urls

--- base/base/urls.py
+++ base/base/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from core.features.versions.v1p0.create_parents.views.create_parents_views import CreateParentViews
+from core.features.versions.v1p0.create_pets.views.create_pets_views import CreatePetsViews
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('create_pets/', CreatePetsViews.as_view(), name ='create_pets_view'),
+    path('create_parents/', CreateParentViews.as_view(), name ='create_pets_view')
 ]
