6c4a0dd3936008869618017214b8c0a88e15f258
johncarls <salaojohn4@gmail.com>
2024-03-10 15:45:56 +0800

    allow_null = True added in serializers

--- base/core/features/versions/v1p0/create_medical_history_with_pet_id_as_parameter/serializers/create_medical_history_views_with_id_serializers.py
+++ base/core/features/versions/v1p0/create_medical_history_with_pet_id_as_parameter/serializers/create_medical_history_views_with_id_serializers.py
@@ -2,8 +2,7 @@ from rest_framework import serializers
 from core.models import Parent, Pets, MedicalHistory
 from base.utilities.formatter import CustomDateFormatField
 from base.utilities.helpers import validate_past, validate_future
-
-
+import datetime
 class NullableFloatField(serializers.FloatField):
     def to_internal_value(self, value):
         if value == "":
@@ -11,12 +10,13 @@ class NullableFloatField(serializers.FloatField):
         return super().to_internal_value(value)
 class CreateMedicalHistoryWithIDSerializer(serializers.ModelSerializer):
     pet = serializers.CharField(read_only=True)
-    last_vaccination_date = CustomDateFormatField(required=False,  validators=[validate_past])
-    last_deworming_date = CustomDateFormatField(required=False,  validators=[validate_past])
-    date_hospitalized = CustomDateFormatField(required=False,  validators=[validate_past])
-    followup_checkup_date = CustomDateFormatField(required=False,  validators=[validate_future])
+    last_vaccination_date = CustomDateFormatField(required=False, allow_null = True,  validators=[validate_past])
+    last_deworming_date = CustomDateFormatField(required=False, allow_null = True, validators=[validate_past])
+    date_hospitalized = CustomDateFormatField(required=False,allow_null = True,  validators=[validate_past])
+    followup_checkup_date = CustomDateFormatField(required=False, allow_null = True, validators=[validate_future])
     initial_temp = NullableFloatField(required=False,)
     weight = NullableFloatField(required=False, )
+    
     class Meta:
         model = MedicalHistory
         fields = [
