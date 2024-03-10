206beb53fbc3af85901bb48631913d69588b8615
johncarls <salaojohn4@gmail.com>
2024-03-10 13:58:33 +0800

    customized formatter

--- base/base/utilities/formatter.py
+++ base/base/utilities/formatter.py
@@ -4,7 +4,7 @@ import datetime
 class CustomDateFormatField(serializers.DateField):
     def to_internal_value(self, value):
         if value == "":
-            return ""  # Return None for empty strings
+            return None  # Return None for empty strings
         try:
             # Parse the input date in 'YYYY/MM/DD' format
             date_obj = datetime.datetime.strptime(value, '%Y/%m/%d').date()
